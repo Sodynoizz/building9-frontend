91056a7ff6c64bb584a9b5fffbd060ab71131889
Nowath <nanoprocom@gmail.com>
2023-10-14 02:29:32 +0700

    Homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,55 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import white from './assets/white.jpg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='main-content'>
+      <nav>
+
+      </nav>
+      <div className="page">
+        <div className="home">
+          
+        </div>
+        <div className="public">
+          <h1>ข่าวประชาสัมพันธ์</h1>
+          <div className="Catalog">
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+          </div>
+          <button><p>อ่านทั้งหมด</p></button>
+        </div>
+        <div className="activity">
+          <h1>กิจกรรม</h1>
+          <div className="Catalog">
+            <div className="item"></div>
+            <div className="item"></div>
+            <div className="item"></div>
+          </div>
+          <button><p>ดูทั้งหมด</p></button>
+        </div>
+        <div className="road">
+          <div className="Catalog">
+              <div className="item"><a href=""><img src={white} alt="" /></a><p>สารสนเทศ</p></div>
+              <div className="item"><a href="https://schedule.tucm.cc/"><img src={white} alt="" /></a><p>ตารางเรียน</p></div>
+              <div className="item"><a href=""><img src={white} alt="" /></a><p>ปฏิทิน</p></div>
+              <div className="item"><a href=""><img src={white} alt="" /></a><p>ลงทะเบียน</p></div>
+              <div className="item"><a href=""><img src={white} alt="" /></a><p>ประกาศ</p></div>
+              <div className="item"><a href=""><img src={white} alt="" /></a><p>ระบบร้องเรียน</p></div>
+            </div>
+        </div>
+        <footer></footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
